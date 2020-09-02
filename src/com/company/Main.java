39c762c00c58c62214dbab6b35a4fbbe2a43ecb9
ADMIN <patelparth31795@gmail.com>
2020-09-02 09:28:00 +0530

    sum three value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.print("sum :");
+        int number1 = 10;
+        int number2 = 7;
+        int number3 = 5;
+        int sum = number1 + number2 + number3;
+        System.out.println(sum);
     }
 }
