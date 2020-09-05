aa472204508a0516f113e1d09ad31aae72fd99f1
ADMIN <patelparth31795@gmail.com>
2020-09-05 10:36:26 +0530

    4 5 6

---
+++ src/com/company/CWH_04_Literals.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CWH_04_Literals {
+    public static void main(String[] args) {
+
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d = 4.66D;
+        boolean a = true;
+        String str = "Parth";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ageDino);
+        System.out.println(f1);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(a);
+        System.out.println(str);
+
+
+
+    }
+
+}
---
+++ src/com/company/CWH_06_Quiz.java
@@ -0,0 +1,53 @@
+/*
+*
+*
+*
+*
+* */
+package com.company;
+import  java.util.Scanner;
+
+public class CWH_06_Quiz {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        float totalmarks = 0;
+        System.out.println("Enter Student Marks out of 100");
+
+        System.out.println("Enter Java Marks");
+        float Java = sc.nextFloat();
+
+        System.out.println("Enter JavaScript Marks");
+        float JS = sc.nextFloat();
+
+        System.out.println("Enter python Marks");
+        float Python = sc.nextFloat();
+
+        System.out.println("Enter php Marks");
+        float Php = sc.nextFloat();
+
+        System.out.println("Enter c++ Marks");
+        float Cpp = sc.nextFloat();
+
+        totalmarks = Java + JS + Python + Php + Cpp;
+
+        float percentage = (totalmarks * 100) / 500;
+
+        System.out.println("Your TotalMarks out of 500");
+        System.out.println(totalmarks);
+        System.out.println("Your percentage out of 500");
+        System.out.println(percentage);
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
